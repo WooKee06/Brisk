424890faa3f46beda06eccc69113671efc4fbf19
WooKee06 <144827180+WooKee06@users.noreply.github.com>
2024-04-19 11:12:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const burger = document.getElementById("burger")
+const burgerOpen = document.querySelector(".burger-img")
+const burgerClose = document.getElementById("btn-close")
+
+
+function openBur () {
+  burger.style.display = "block"
+}
+function closeBur () {
+  burger.style.display = "none"
+}
